2af18c7a93933ea2c1d099c92870cc921ee32f75
Víctor Martínez <victorandresmtz@gmail.com>
2020-04-14 15:02:36 -0500

    feat(app): install Apollo for GraphQL requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'luminskin-code-challenge';
+export class AppComponent implements OnInit {
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    // this.apollo
+    //   .watchQuery({
+    //     query: gql`
+    //       {
+    //         currency
+    //       }
+    //     `,
+    //   })
+    //   .valueChanges.subscribe((value) => console.log(value));
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  graphqlUrl: 'https://pangaea-interviews.now.sh/api/graphql'
 };
